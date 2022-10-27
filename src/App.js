87507f80c77b962234f5e83e273140b30325ff0a
rpraneeth <rpraneeth@mirabeltechnologies.com>
2022-10-27 13:44:27 +0530

    Scaffolding changes for Medipath

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Suspense, lazy } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const SignIn = lazy(() => import("./components/SignInSide"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
+
+const App = () => (
+  <>
+    <CssBaseline />
+    <Router>
+      <Suspense fallback={<CircularProgress />}>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const darkThemeOptions = {
+  palette: {
+    mode: "dark",
+  },
+};
+
+const theme = createTheme(darkThemeOptions);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
